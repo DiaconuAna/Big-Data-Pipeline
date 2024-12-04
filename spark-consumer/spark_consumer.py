abf7a8f6dc172e822757c0dd78034b3a42fbc931
Diaconu Ana-Maria <aminad41@gmail.com>
2024-12-05 01:34:15 +0200

    Big Data Pipeline: connected spark consumer to cassandra
    -> sensor id is 1
    -> commands to check the database updating (docker container)
    
    docker exec -it cassandra-iot bash // open docker container
    // apply schema.cql if container is newly created (-f ./schema.cql)
    cqlsh 172.22.0.6 -u cassandra -p cassandra 9042
    // check keyspaces electrical exists (first time)
    describe keyspaces;
    // see records from table
    use electrical;
    select * from sensor_data;

--- spark-consumer/spark_consumer.py
+++ spark-consumer/spark_consumer.py
@@ -1,17 +1,21 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json
-from pyspark.sql.types import StructType, StructField, DoubleType, LongType
+from pyspark.sql.functions import col, from_json, lit
+from pyspark.sql.types import StructType, StructField, DoubleType, LongType, StringType
 
 # Initialize Spark session
 spark = SparkSession.builder \
     .appName("KafkaConsumer") \
+    .config("spark.cassandra.connection.host", "cassandra") \
+    .config("spark.cassandra.connection.port", "9042") \
+    .config("spark.cassandra.auth.username", "cassandra") \
+    .config("spark.cassandra.auth.password", "cassandra") \
     .getOrCreate()
 
-# Define Kafka server and topic
-kafka_bootstrap_servers = "kafka:9092"  # Adjust if necessary
+# Kafka server and topic
+kafka_bootstrap_servers = "kafka:9092"
 topic = "electrical_read"
 
-# Define the schema for the JSON message based on the producer
+# Schema for the JSON message based on the producer
 schema = StructType([
     StructField("time", LongType(), True),
     StructField("global_active_power", DoubleType(), True),
@@ -35,12 +39,21 @@ parsed_df = kafka_df.select(
     from_json(col("value").cast("string"), schema).alias("data")
 ).select("data.*")
 
-# Write the results to the console for testing
-query = parsed_df.writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .start()
+parsed_df_with_id = parsed_df.withColumn("sensor_id", lit(1))
 
+# Write the results to the console for testing
+# query = parsed_df.writeStream \
+#     .outputMode("append") \
+#     .format("console") \
+#     .start()
+
+query = parsed_df_with_id.writeStream \
+    .format("org.apache.spark.sql.cassandra") \
+    .option("checkpointLocation", "/tmp/spark/checkpoint") \
+    .option("keyspace", "electrical") \
+    .option("table", "sensor_data") \
+    .outputMode("append") \
+    .start()
 
 # Await termination
 query.awaitTermination()
