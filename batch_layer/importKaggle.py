85ce4b7d29f3f7f2986a64b0736d9e8c3e8adf72
HoriaRaduRusu <72196149+HoriaRaduRusu@users.noreply.github.com>
2025-01-12 19:02:30 +0200

    Updated import script

--- batch_layer/importKaggle.py
+++ batch_layer/importKaggle.py
@@ -5,11 +5,12 @@ import os
 #  docker cp .\electrical_data\ docker_name:/tmp/electrical_data
 # In the docker terminal, run this to upload files to hdfs
 #  hadoop fs -mkdir electrical_data
-#  hadoop fs -put /tmp/electrical_data/* electrical_data
+#  hadoop fs -put /tmp/electrical_data/* /electrical_data
 
 csv_file_path = 'household_power_consumption.csv'
 df = pd.read_csv(csv_file_path)
 
+df['timestamp'] = pd.to_datetime(df['Date'].astype(str) + ' ' + df['Time'].astype(str), dayfirst=True)
 df['Global_active_power'] = pd.to_numeric(df['Global_active_power'], errors='coerce')
 df['Global_reactive_power'] = pd.to_numeric(df['Global_reactive_power'], errors='coerce')
 df['Voltage'] = pd.to_numeric(df['Voltage'], errors='coerce')
@@ -20,14 +21,11 @@ df['Sub_metering_3'] = pd.to_numeric(df['Sub_metering_3'], errors='coerce')
 
 df.dropna(inplace=True)
 
-df['timestamp'] = pd.to_datetime(df['Date'].astype(str) + ' ' + df['Time'].astype(str), dayfirst=True)
-
 for hour, group in df.groupby(df['timestamp'].dt.floor('T')):
     # Construct the output directory path (partitioned by year, month, day, hour, and minute)
     output_dir_path = f'electrical_data/year={hour.strftime("%Y")}/month={hour.strftime("%m")}/day={hour.strftime("%d")}/hour={hour.strftime("%H")}/minute={hour.strftime("%M")}'
     os.makedirs(output_dir_path, exist_ok=True)
     output_file_path = f'{output_dir_path}/file.parquet'
-    group.drop(columns=['timestamp'], inplace=True)
 
     group.to_parquet(output_file_path, engine='pyarrow', index=False)
 
